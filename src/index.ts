0147c53e1905c1b9e9fb4eebf043299ff10af4f3
Michael Osahon <michaelosahon@Michaels-MacBook-Air.local>
2025-05-13 16:18:52 +0100

    bug fix: export other services like token for consumption

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from './termii-sdk/termii-sdk.module';
 export * from './services/messaging';
+export * from './services/sender';
+export * from './services/number';
+export * from './services/token';
